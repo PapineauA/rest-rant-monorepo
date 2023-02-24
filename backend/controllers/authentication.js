2e14d7b52d22b9db7b42a28ef6738a5390895fee
PapineauA <papineautaidan@gmail.com>
2023-02-23 19:37:51 -0500

    Uploading Session Auth

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -6,9 +6,7 @@ const { User } = db;
 
 router.post("/", async (req, res) => {
   let user = await User.findOne({
-    where: {
-      email: req.body.email,
-    },
+    where: { email: req.body.email },
   });
 
   if (
@@ -19,7 +17,22 @@ router.post("/", async (req, res) => {
       message: `Could not find a user with the provided username and password`,
     });
   } else {
+    req.session.userId = user.userId;
     res.json({ user });
+  }
+});
+
+router.get("/profile", async (req, res) => {
+  console.log(req.session.userId);
+  try {
+    let user = await User.findOne({
+      where: {
+        userId: req.session.userId,
+      },
+    });
+    res.json(user);
+  } catch {
+    res.json(null);
   }
 });
 
